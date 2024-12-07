229f38e81e85d0752ff06f22998aeef4aa880be8
Amir Saatchi <Amir17252@gmail.com>
2024-12-07 13:40:38 +0330

    adding tanstack

---
+++ provider/TanstackProvider.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
+function TanstackProvider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 5,
+            retryDelay: 1000,
+            staleTime:1 * 60 * 60 * 1000, // 1 hour
+            refetchOnMount: false,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+            gcTime: 1 * 60 * 60 * 1000, // 1 hour
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default TanstackProvider;
