98e4a42f6dbb6f5d67ace323dba0cbe1284dc8ff
Amir Saatchi <Amir17252@gmail.com>
2025-01-19 18:08:31 +0330

    set-prisma-to-5.20

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "standalone" };
+const nextConfig = {
+    reactStrictMode: true,
+    i18n: {
+      locales: ["fa"],
+      defaultLocale: "fa",
+    },
+    output: "standalone",
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'trustseal.enamad.ir',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'storage.c2.liara.space',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  };
 
 export default nextConfig;
